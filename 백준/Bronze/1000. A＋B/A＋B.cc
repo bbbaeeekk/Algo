2bf46efab63490499c212b225926b455c042a485
bbbaeeekk <bbbaeeekk@kakao.com>
2023-04-19 08:12:37 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    
+    int A;
+    int B;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    printf("%d", A+B);
+    return 0;
+}
