c5483d0be67f6841081e2b38ec9b69494114e87f
NOONG ++ <bbbaeeekk@kakao.com>
2023-09-04 23:29:11 +0900

    [Gold III] Title: 줄 세우기, Time: 4164 ms, Memory: 38072 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+indegree = [0] * (N+1)
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    indegree[y]+=1
+
+# 위상 정렬
+
+result = []
+q = deque()
+
+for i in range(1,N+1):
+    if indegree[i]==0:
+        q.append(i)
+
+while q:
+    a = q.popleft()
+    result.append(a)
+
+    for i in graph[a]:
+        indegree[i]-=1
+
+        if indegree[i]==0:
+            q.append(i)
+for i in result:
+    print(i,end=' ')
