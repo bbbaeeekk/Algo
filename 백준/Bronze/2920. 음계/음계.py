3322554ce4384b57e3efee25573bfc7ce81d3416
NOONG ++ <bbbaeeekk@kakao.com>
2023-09-27 02:24:00 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+arr = list(map(int,input().split()))
+
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
+
+if arr == ascending:
+    print('ascending')
+elif arr == descending:
+    print('descending')
+else:
+    print('mixed')
