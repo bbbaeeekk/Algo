b994851abed9db0f17758e25ca2e9c68d7953a59
NOONG ++ <bbbaeeekk@kakao.com>
2023-10-20 15:41:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 1036 ms, Memory: 79600 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -5,10 +5,7 @@ n, k = map(int,input().split())
 q = deque([[n,0]])
 visited = [False]*100001
 
-def inragne(x):
-    if 0 <= x <= 100000:
-        return True
-    return False
+
 
 while q:
     s,t = q.popleft()
@@ -18,8 +15,5 @@ while q:
         break
 
     for a in [s-1,s+1,s*2]:
-
-        if inragne(a) and not visited[a]:
+        if  0 <= a <= 100000 and not visited[a]:
             q.append([a,t+1])
-    a = s-1
-
