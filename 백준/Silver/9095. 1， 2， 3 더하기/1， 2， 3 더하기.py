07da91e597db10b46391b88fa586b8fd1b40fa6a
NOONG ++ <bbbaeeekk@kakao.com>
2023-10-20 16:36:57 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+dp =[0] * 12
+
+dp[1]=1
+dp[2]=2
+dp[3]=4
+
+for i in range(4,12):
+    dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+
+n = int(input())
+for _ in range(n):
+    a = int(input())
+    print(dp[a])
